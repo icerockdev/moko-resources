61b4b4058511872d80f9f1d86515f1b075be2a2a
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-05-04 11:37:03 +0700

    #50 fix compilation with new gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,23 @@
  * Copyright 2019 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
 
+buildscript {
+    repositories {
+        mavenLocal()
+
+        jcenter()
+        google()
+
+        maven { url = uri("https://dl.bintray.com/kotlin/kotlin") }
+        maven { url = uri("https://kotlin.bintray.com/kotlinx") }
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://dl.bintray.com/icerockdev/plugins") }
+    }
+    dependencies {
+        Deps.plugins.values.forEach { classpath(it) }
+    }
+}
+
 allprojects {
     repositories {
         mavenLocal()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,25 +2,6 @@
  * Copyright 2019 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
 
-pluginManagement {
-    repositories {
-        mavenLocal()
-
-        jcenter()
-        google()
-
-        maven { url = uri("https://dl.bintray.com/kotlin/kotlin") }
-        maven { url = uri("https://kotlin.bintray.com/kotlinx") }
-        maven { url = uri("https://plugins.gradle.org/m2/") }
-        maven { url = uri("https://dl.bintray.com/icerockdev/plugins") }
-    }
-    resolutionStrategy.eachPlugin {
-        val module = Deps.plugins[requested.id.id] ?: return@eachPlugin
-
-        useModule(module)
-    }
-}
-
 enableFeaturePreview("GRADLE_METADATA")
 
 val properties = startParameter.projectProperties
