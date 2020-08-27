b0d662a4bd31154d0dc786a685551d03eff0f01b
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-08-27 20:16:21 +0700

    fix detekt problems

--- resources/src/commonMain/kotlin/dev/icerock/moko/resources/desc/PluralStringDesc.kt
+++ resources/src/commonMain/kotlin/dev/icerock/moko/resources/desc/PluralStringDesc.kt
@@ -9,4 +9,5 @@ import dev.icerock.moko.resources.PluralsResource
 
 @Suppress("FunctionName")
 fun StringDesc.Companion.Plural(pluralsRes: PluralsResource, number: Int) = PluralStringDesc(pluralsRes, number)
+
 expect class PluralStringDesc(pluralsRes: PluralsResource, number: Int) : StringDesc, Parcelable
