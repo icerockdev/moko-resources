0d9cdb81ea3b96da1fde9c40c9efdb2490c84fae
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-09-05 13:35:30 +0700

    #99 add copyFrameworkResourcesToApp task for static frameworks resources integration

--- gradle-plugin/src/main/kotlin/dev/icerock/gradle/tasks/GenerateMultiplatformResourcesTask.kt
+++ gradle-plugin/src/main/kotlin/dev/icerock/gradle/tasks/GenerateMultiplatformResourcesTask.kt
@@ -2,12 +2,12 @@
  * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package dev.icerock.gradle.generator
+package dev.icerock.gradle.tasks
 
 import org.gradle.api.DefaultTask
 
 open class GenerateMultiplatformResourcesTask : DefaultTask() {
     init {
-        group = "multiplatform"
+        group = "moko-resources"
     }
 }
