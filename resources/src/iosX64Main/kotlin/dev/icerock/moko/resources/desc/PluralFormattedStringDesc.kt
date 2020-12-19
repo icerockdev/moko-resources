d462cb33d98135eb201a23dcdb95ad6ca6d2d939
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-12-20 00:03:26 +0700

    #123 copyrights

--- resources/src/iosX64Main/kotlin/dev/icerock/moko/resources/desc/PluralFormattedStringDesc.kt
+++ resources/src/iosX64Main/kotlin/dev/icerock/moko/resources/desc/PluralFormattedStringDesc.kt
@@ -1,6 +1,7 @@
 /*
  * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
+
 package dev.icerock.moko.resources.desc
 
 import dev.icerock.moko.resources.PluralsResource
--- resources/src/macosX64Main/kotlin/dev/icerock/moko/resources/ResourceContainerExt.kt
+++ resources/src/macosX64Main/kotlin/dev/icerock/moko/resources/ResourceContainerExt.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
 package dev.icerock.moko.resources
 
 actual fun ResourceContainer<ImageResource>.getImageByFileName(fileName: String): ImageResource? {
