f206179debb94fa009b4afb15163f156b0c6833c
tetraquark <avladislavv@gmail.com>
2021-02-02 14:17:52 +0700

    #163: create ImageDesc, ImageDescResource, ImageDescUrl

---
+++ resources/src/commonMain/kotlin/dev/icerock/moko/resources/desc/image/ImageDescResource.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2021 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package dev.icerock.moko.resources.desc.image
+
+import dev.icerock.moko.resources.ImageResource
+
+class ImageDescResource(val resource: ImageResource) : ImageDesc
+
+@Suppress("FunctionName")
+fun ImageDesc.Companion.Resource(
+    imageResource: ImageResource
+): ImageDesc = ImageDescResource(imageResource)
+
+fun ImageResource.asImageDesc() = ImageDesc.Resource(this)
