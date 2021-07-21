b46a9ccaf22196ad0ac00170a0e734ba683356d4
Anton Shestak <ashestak@icerockdev.com>
2021-07-21 16:59:20 +0700

    #220 added empty-lines for more readable

--- sample/android-app-compose/build.gradle.kts
+++ sample/android-app-compose/build.gradle.kts
@@ -1,6 +1,7 @@
 /*
  * Copyright 2021 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
+
 plugins {
     id("android-app-convention")
     id("dev.icerock.mobile.multiplatform-resources")
