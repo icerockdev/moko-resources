aae57445e63f5baf431fe076f63b6e17f6c319cb
Anton Shestak <ashestak@icerockdev.com>
2021-08-03 13:38:10 +0700

    #197 publish plugin to gradlePluginPortal

--- resources-generator/build.gradle.kts
+++ resources-generator/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version ("1.5.20")
     id("detekt-convention")
     id("publication-convention")
+    id("com.gradle.plugin-publish") version ("0.15.0")
+    id("java-gradle-plugin")
 }
 
 group = "dev.icerock.moko"
@@ -34,4 +36,32 @@ publishing.publications.register("mavenJava", MavenPublication::class) {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+gradlePlugin {
+    plugins {
+        create("multiplatform-resources") {
+            id = "dev.icerock.mobile.multiplatform-resources"
+            implementationClass = "dev.icerock.gradle.MultiplatformResourcesPlugin"
+        }
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/icerockdev/moko-resources"
+    vcsUrl = "https://github.com/icerockdev/moko-resources"
+    description = "Plugin to provide access to the resources on iOS & Android"
+    tags = listOf("moko-resources", "moko", "kotlin", "kotlin-multiplatform")
+
+    plugins {
+        getByName("multiplatform-resources") {
+            displayName = "MOKO resources generator plugin"
+        }
+    }
+
+    mavenCoordinates {
+        groupId = project.group as String
+        artifactId = project.name
+        version = project.version as String
+    }
 }
