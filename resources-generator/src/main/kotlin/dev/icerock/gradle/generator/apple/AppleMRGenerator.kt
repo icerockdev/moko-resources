824edc19e4b4287c55b42a95626684bd8dfea86f
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2022-01-14 14:33:21 +0700

    #269 detekt fix

--- resources-generator/src/main/kotlin/dev/icerock/gradle/generator/apple/AppleMRGenerator.kt
+++ resources-generator/src/main/kotlin/dev/icerock/gradle/generator/apple/AppleMRGenerator.kt
@@ -35,7 +35,6 @@ import java.util.Properties
 import java.util.zip.ZipEntry
 import java.util.zip.ZipException
 import java.util.zip.ZipFile
-import kotlin.streams.asStream
 
 @Suppress("TooManyFunctions")
 class AppleMRGenerator(
