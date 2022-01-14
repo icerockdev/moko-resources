76fd30cca723eeda1c0dd8bf26568ed2d826f010
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2022-01-14 17:30:21 +0700

    #280 fix invalid plurals with StringDesc.localeType usage on apple

--- sample/mpp-library/src/commonTest/kotlin/com/icerock/library/StringResourceTests.kt
+++ sample/mpp-library/src/commonTest/kotlin/com/icerock/library/StringResourceTests.kt
@@ -47,7 +47,7 @@ class StringResourceTests: BaseUnitTest() {
         val rawString: String = Testing.getStrings().joinToString("\n") { it.getString() }
 
         assertEquals(
-            expected = "тест\nтест 2\nтест 3\nТестовый проект\nsome raw string\nмного\nодин\n" +
+            expected = "тест\nтест 2\nтест 3\nТестовый проект\nsome raw string\nодин\n" +
                     "несколько\nнесколько\nпервая строка\nвторая строка\nтретья строка.\n" +
                     "Alex009 сказал \"привет мир\" & \"пишите тесты\".\nтест вложенный\nмного",
             actual = rawString
@@ -60,9 +60,85 @@ class StringResourceTests: BaseUnitTest() {
         val rawString: String = Testing.getStrings().joinToString("\n") { it.getString() }
 
         assertEquals(
-            expected = "test\ntest 2\ntest 3\nTest Project\nsome raw string\nother\none\nother\n" +
+            expected = "test\ntest 2\ntest 3\nTest Project\nsome raw string\none\nother\n" +
                     "other\nfirst line\nsecond line\nthird line.\n" +
                     "Alex009 said \"hello world\" & \"write tests\".\nnested test\nother",
+            actual = rawString
+        )
+    }
+
+    @Test
+    fun `check ru plurals`() {
+        StringDesc.localeType = StringDesc.LocaleType.Custom("ru")
+        val rawString: String = Testing.getPlurals().getString()
+
+        assertEquals(
+            expected = """
+                1 элемент
+                2 элемента
+                3 элемента
+                4 элемента
+                5 элементов
+                6 элементов
+                7 элементов
+                8 элементов
+                9 элементов
+                10 элементов
+                11 элементов
+                12 элементов
+                13 элементов
+                14 элементов
+                15 элементов
+                16 элементов
+                17 элементов
+                18 элементов
+                19 элементов
+                20 элементов
+                21 элемент
+                22 элемента
+                23 элемента
+                24 элемента
+                25 элементов
+                26 элементов
+            """.trimIndent(),
+            actual = rawString
+        )
+    }
+
+    @Test
+    fun `check en plurals`() {
+        StringDesc.localeType = StringDesc.LocaleType.Custom("en")
+        val rawString: String = Testing.getPlurals().getString()
+
+        assertEquals(
+            expected = """
+                1 item
+                2 items
+                3 items
+                4 items
+                5 items
+                6 items
+                7 items
+                8 items
+                9 items
+                10 items
+                11 items
+                12 items
+                13 items
+                14 items
+                15 items
+                16 items
+                17 items
+                18 items
+                19 items
+                20 items
+                21 items
+                22 items
+                23 items
+                24 items
+                25 items
+                26 items
+            """.trimIndent(),
             actual = rawString
         )
     }
