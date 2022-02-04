29bdb30d89870e30bf0f34017f2cd4571771aed8
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2022-02-04 22:44:37 +0700

    #272 simple plurals test

--- sample/mpp-library/src/commonTest/kotlin/com/icerock/library/StringResourceTests.kt
+++ sample/mpp-library/src/commonTest/kotlin/com/icerock/library/StringResourceTests.kt
@@ -46,7 +46,71 @@ class StringResourceTests : BaseUnitTest() {
     }
 
     @Test
+    fun checkRuPlural() = runTest {
+        MR.plurals.download()
+
+        StringDesc.localeType = StringDesc.LocaleType.Custom("ru")
+
+        assertEquals(
+            expected = "ноль",
+            actual = MR.plurals.test_plural.desc(0).getString()
+        )
+        assertEquals(
+            expected = "один",
+            actual = MR.plurals.test_plural.desc(1).getString()
+        )
+        assertEquals(
+            expected = "два",
+            actual = MR.plurals.test_plural.desc(2).getString()
+        )
+        assertEquals(
+            expected = "несколько",
+            actual = MR.plurals.test_plural.desc(3).getString()
+        )
+        assertEquals(
+            expected = "много",
+            actual = MR.plurals.test_plural.desc(40).getString()
+        )
+        assertEquals(
+            expected = "несколько",
+            actual = MR.plurals.test_plural.desc(22).getString()
+        )
+    }
+
+    @Test
+    fun checkEnPlural() = runTest {
+        MR.plurals.download()
+
+        StringDesc.localeType = StringDesc.LocaleType.Custom("en")
+
+        assertEquals(
+            expected = "zero",
+            actual = MR.plurals.test_plural.desc(0).getString()
+        )
+        assertEquals(
+            expected = "one",
+            actual = MR.plurals.test_plural.desc(1).getString()
+        )
+        assertEquals(
+            expected = "two",
+            actual = MR.plurals.test_plural.desc(2).getString()
+        )
+        assertEquals(
+            expected = "other",
+            actual = MR.plurals.test_plural.desc(3).getString()
+        )
+        assertEquals(
+            expected = "other",
+            actual = MR.plurals.test_plural.desc(40).getString()
+        )
+        assertEquals(
+            expected = "other",
+            actual = MR.plurals.test_plural.desc(22).getString()
+        )
+    }
+
     @Ignore
+    @Test
     fun checkRuStrings() = runTest {
         MR.strings.download()
         MR.plurals.download()
@@ -62,6 +126,7 @@ class StringResourceTests : BaseUnitTest() {
         )
     }
 
+    @Ignore
     @Test
     fun checkEnStrings() = runTest {
         MR.strings.download()
@@ -78,107 +143,111 @@ class StringResourceTests : BaseUnitTest() {
         )
     }
 
-//    @Test
-//    fun checkRuStringsList() {
-//        StringDesc.localeType = StringDesc.LocaleType.Custom("ru")
-//        val rawString: String = Testing.getStrings().joinToString("\n") { it.getString() }
-//
-//        assertEquals(
-//            expected = "тест\nтест 2\nтест 3\nТестовый проект\nsome raw string\nодин\n" +
-//                    "несколько\nнесколько\nпервая строка\nвторая строка\nтретья строка.\n" +
-//                    "Alex009 сказал \"привет мир\" & \"пишите тесты\".\nтест вложенный\nмного\n" +
-//                    "plurals-interop: один\nplurals-interop: несколько\nplurals-interop: много",
-//            actual = rawString
-//        )
-//    }
-//
-//    @Test
-//    fun checkEnStringsList() {
-//        StringDesc.localeType = StringDesc.LocaleType.Custom("en")
-//        val rawString: String = Testing.getStrings().joinToString("\n") { it.getString() }
-//
-//        assertEquals(
-//            expected = "test\ntest 2\ntest 3\nTest Project\nsome raw string\none\nother\n" +
-//                    "other\nfirst line\nsecond line\nthird line.\n" +
-//                    "Alex009 said \"hello world\" & \"write tests\".\nnested test\nother\n" +
-//                    "plurals-interop: one\nplurals-interop: other\nplurals-interop: other",
-//            actual = rawString
-//        )
-//    }
-//
-//    @Test
-//    fun checkRuPlurals() {
-//        StringDesc.localeType = StringDesc.LocaleType.Custom("ru")
-//        val rawString: String = Testing.getPlurals().getString()
-//
-//        assertEquals(
-//            expected = """
-//                1 элемент
-//                2 элемента
-//                3 элемента
-//                4 элемента
-//                5 элементов
-//                6 элементов
-//                7 элементов
-//                8 элементов
-//                9 элементов
-//                10 элементов
-//                11 элементов
-//                12 элементов
-//                13 элементов
-//                14 элементов
-//                15 элементов
-//                16 элементов
-//                17 элементов
-//                18 элементов
-//                19 элементов
-//                20 элементов
-//                21 элемент
-//                22 элемента
-//                23 элемента
-//                24 элемента
-//                25 элементов
-//                26 элементов
-//            """.trimIndent(),
-//            actual = rawString
-//        )
-//    }
-//
-//    @Test
-//    fun checkEnPlurals() {
-//        StringDesc.localeType = StringDesc.LocaleType.Custom("en")
-//        val rawString: String = Testing.getPlurals().getString()
-//
-//        assertEquals(
-//            expected = """
-//                1 item
-//                2 items
-//                3 items
-//                4 items
-//                5 items
-//                6 items
-//                7 items
-//                8 items
-//                9 items
-//                10 items
-//                11 items
-//                12 items
-//                13 items
-//                14 items
-//                15 items
-//                16 items
-//                17 items
-//                18 items
-//                19 items
-//                20 items
-//                21 items
-//                22 items
-//                23 items
-//                24 items
-//                25 items
-//                26 items
-//            """.trimIndent(),
-//            actual = rawString
-//        )
-//    }
+    @Ignore
+    @Test
+    fun checkRuStringsList() {
+        StringDesc.localeType = StringDesc.LocaleType.Custom("ru")
+        val rawString: String = Testing.getStrings().joinToString("\n") { it.getString() }
+
+        assertEquals(
+            expected = "тест\nтест 2\nтест 3\nТестовый проект\nsome raw string\nодин\n" +
+                    "несколько\nнесколько\nпервая строка\nвторая строка\nтретья строка.\n" +
+                    "Alex009 сказал \"привет мир\" & \"пишите тесты\".\nтест вложенный\nмного\n" +
+                    "plurals-interop: один\nplurals-interop: несколько\nplurals-interop: много",
+            actual = rawString
+        )
+    }
+
+    @Ignore
+    @Test
+    fun checkEnStringsList() {
+        StringDesc.localeType = StringDesc.LocaleType.Custom("en")
+        val rawString: String = Testing.getStrings().joinToString("\n") { it.getString() }
+
+        assertEquals(
+            expected = "test\ntest 2\ntest 3\nTest Project\nsome raw string\none\nother\n" +
+                    "other\nfirst line\nsecond line\nthird line.\n" +
+                    "Alex009 said \"hello world\" & \"write tests\".\nnested test\nother\n" +
+                    "plurals-interop: one\nplurals-interop: other\nplurals-interop: other",
+            actual = rawString
+        )
+    }
+
+    @Ignore
+    @Test
+    fun checkRuPlurals() {
+        StringDesc.localeType = StringDesc.LocaleType.Custom("ru")
+        val rawString: String = Testing.getPlurals().getString()
+
+        assertEquals(
+            expected = """
+                1 элемент
+                2 элемента
+                3 элемента
+                4 элемента
+                5 элементов
+                6 элементов
+                7 элементов
+                8 элементов
+                9 элементов
+                10 элементов
+                11 элементов
+                12 элементов
+                13 элементов
+                14 элементов
+                15 элементов
+                16 элементов
+                17 элементов
+                18 элементов
+                19 элементов
+                20 элементов
+                21 элемент
+                22 элемента
+                23 элемента
+                24 элемента
+                25 элементов
+                26 элементов
+            """.trimIndent(),
+            actual = rawString
+        )
+    }
+
+    @Ignore
+    @Test
+    fun checkEnPlurals() {
+        StringDesc.localeType = StringDesc.LocaleType.Custom("en")
+        val rawString: String = Testing.getPlurals().getString()
+
+        assertEquals(
+            expected = """
+                1 item
+                2 items
+                3 items
+                4 items
+                5 items
+                6 items
+                7 items
+                8 items
+                9 items
+                10 items
+                11 items
+                12 items
+                13 items
+                14 items
+                15 items
+                16 items
+                17 items
+                18 items
+                19 items
+                20 items
+                21 items
+                22 items
+                23 items
+                24 items
+                25 items
+                26 items
+            """.trimIndent(),
+            actual = rawString
+        )
+    }
 }
