3da3f3be90946fff607abe37be4017a606ceeede
y9san9 <y9san9@gmail.com>
2022-03-21 20:37:25 +0300

    #315: migrated to version catalog in resource-build-logic

--- resources-build-logic/build.gradle.kts
+++ resources-build-logic/build.gradle.kts
@@ -10,8 +10,8 @@ repositories {
 }
 
 dependencies {
-    api("dev.icerock:mobile-multiplatform:0.13.0")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-    api("com.android.tools.build:gradle:7.0.4")
-    api("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.15.0")
+    api(libs.mokoMultiplatformPlugin)
+    api(libs.kotlinGradlePlugin)
+    api(libs.androidGradlePlugin)
+    api(libs.detektGradlePlugin)
 }
