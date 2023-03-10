6813b6b8b77f19c38987caa391d049e36220829d
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2023-03-11 00:26:31 +0600

    #417 review fixes

--- resources-generator/src/main/kotlin/dev/icerock/gradle/tasks/CopyFrameworkResourcesToAppEntryPointTask.kt
+++ resources-generator/src/main/kotlin/dev/icerock/gradle/tasks/CopyFrameworkResourcesToAppEntryPointTask.kt
@@ -4,7 +4,9 @@
 
 package dev.icerock.gradle.tasks
 
+import dev.icerock.gradle.utils.propertyString
 import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
@@ -18,28 +20,28 @@ open class CopyFrameworkResourcesToAppEntryPointTask : DefaultTask() {
     @get:Internal
     internal val configuration: String?
         get() {
-            val configurationName =
-                (project.findProperty("moko.resources.CONFIGURATION") as? String)
+            val configurationName: String =
+                project.propertyString("kotlin.native.cocoapods.configuration")
+                    ?: project.propertyString("moko.resources.CONFIGURATION")
+                    ?: return null
 
             return configurationMapper[configurationName]?.name ?: configurationName
         }
 
     @get:Internal
-    internal val platformName: String?
-
-    @get:Internal
-    internal val archs: String?
-
-    @get:Internal
     internal var configurationMapper: Map<String, NativeBuildType> = emptyMap()
+
+    private val platformName: String?
+    private val archs: String?
 
     init {
         group = "moko-resources"
 
-        platformName =
-            project.findProperty("moko.resources.PLATFORM_NAME") as? String
+        platformName = project.propertyString("kotlin.native.cocoapods.platform")
+            ?: project.propertyString("moko.resources.PLATFORM_NAME")
 
-        archs = project.findProperty("kotlin.native.cocoapods.archs") as? String
+        archs = project.propertyString("kotlin.native.cocoapods.archs")
+            ?: project.propertyString("moko.resources.ARCHS")
 
         konanTarget = when (platformName) {
             "iphonesimulator" -> when {
@@ -47,12 +49,14 @@ open class CopyFrameworkResourcesToAppEntryPointTask : DefaultTask() {
                 archs?.contains("x86_64") == true -> KonanTarget.IOS_X64
                 else -> null
             }
+
             "iphoneos" -> KonanTarget.IOS_ARM64
             "macosx" -> when {
                 archs?.contains("arm64") == true -> KonanTarget.MACOS_ARM64
                 archs?.contains("x86_64") == true -> KonanTarget.MACOS_X64
                 else -> null
             }
+
             else -> null
         }
     }
@@ -60,9 +64,9 @@ open class CopyFrameworkResourcesToAppEntryPointTask : DefaultTask() {
     @TaskAction
     fun action() {
         if (dependsOn.isEmpty()) {
-            throw IllegalStateException(
+            throw GradleException(
                 """
-framework link task with konanTarget $konanTarget, platform $platformName and buildType $configuration not found!
+framework link task with konanTarget $konanTarget, platform $platformName, arch $archs and buildType $configuration not found!
 """
             )
         }
