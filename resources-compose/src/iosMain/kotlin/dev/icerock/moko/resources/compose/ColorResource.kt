bb49c9ae0894cf51495c863a47de113cc8e93da3
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2023-03-13 23:26:01 +0600

    #154 fix color getter on iOS compose

--- resources-compose/src/iosMain/kotlin/dev/icerock/moko/resources/compose/ColorResource.kt
+++ resources-compose/src/iosMain/kotlin/dev/icerock/moko/resources/compose/ColorResource.kt
@@ -6,21 +6,43 @@ package dev.icerock.moko.resources.compose
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import dev.icerock.moko.resources.ColorResource
 import dev.icerock.moko.resources.getUIColor
-import platform.CoreImage.CIColor
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
+import kotlinx.cinterop.value
+import platform.CoreGraphics.CGFloatVar
+import platform.UIKit.UIColor
 
 @Composable
 actual fun colorResource(resource: ColorResource): Color {
     // read darkmode to call recomposition when system settings will be changed
     val darkMode: Boolean = isSystemInDarkTheme()
-    // FIXME: -CIColor not defined for the UIColor <UIDynamicCatalogColor: 0x60000371b610; name = textColor>; need to first convert colorspace.'
-    val ciColor: CIColor = resource.getUIColor().CIColor
-    return Color(
-        red = ciColor.red.toFloat(),
-        green = ciColor.green.toFloat(),
-        blue = ciColor.blue.toFloat(),
-        alpha = ciColor.alpha.toFloat()
-    )
+    return remember(darkMode) {
+        val uiColor: UIColor = resource.getUIColor()
+
+        memScoped {
+            val red = alloc<CGFloatVar>()
+            val green = alloc<CGFloatVar>()
+            val blue = alloc<CGFloatVar>()
+            val alpha = alloc<CGFloatVar>()
+
+            uiColor.getRed(
+                red = red.ptr,
+                green = green.ptr,
+                blue = blue.ptr,
+                alpha = alpha.ptr
+            )
+
+            Color(
+                red = red.value.toFloat(),
+                green = green.value.toFloat(),
+                blue = blue.value.toFloat(),
+                alpha = alpha.value.toFloat()
+            )
+        }
+    }
 }
