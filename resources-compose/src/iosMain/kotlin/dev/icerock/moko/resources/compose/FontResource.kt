e210f6c1b4148f88f07e76a187073fff4c456a7f
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2023-03-23 18:57:10 +0600

    #440 ios font in compose

--- resources-compose/src/iosMain/kotlin/dev/icerock/moko/resources/compose/FontResource.kt
+++ resources-compose/src/iosMain/kotlin/dev/icerock/moko/resources/compose/FontResource.kt
@@ -5,10 +5,31 @@
 package dev.icerock.moko.resources.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.platform.Font
 import dev.icerock.moko.resources.FontResource
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.usePinned
+import platform.Foundation.NSData
+import platform.posix.memcpy
 
 @Composable
 actual fun fontFamilyResource(fontResource: FontResource): FontFamily {
-    TODO()
+    return remember(fontResource) {
+        val font = Font(
+            identity = fontResource.fontName,
+            data = fontResource.data.toByteArray()
+        )
+
+        FontFamily(font)
+    }
+}
+
+private fun NSData.toByteArray(): ByteArray {
+    return ByteArray(this@toByteArray.length.toInt()).apply {
+        usePinned { pinned ->
+            memcpy(pinned.addressOf(0), this@toByteArray.bytes, this@toByteArray.length)
+        }
+    }
 }
