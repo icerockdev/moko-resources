9244eb5d2515b4683ad29593dda700274ea0cbe2
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2023-03-24 16:50:09 +0600

    update compose multiplatform sample app

--- samples/compose-resources-gallery/shared/src/commonMain/kotlin/App.kt
+++ samples/compose-resources-gallery/shared/src/commonMain/kotlin/App.kt
@@ -1,64 +1,70 @@
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
 import com.icerockdev.library.MR
-import dev.icerock.moko.resources.AssetResource
-import dev.icerock.moko.resources.FileResource
-import dev.icerock.moko.resources.compose.colorResource
-import dev.icerock.moko.resources.compose.painterResource
-import dev.icerock.moko.resources.compose.readTextAsState
-import dev.icerock.moko.resources.compose.stringResource
-import dev.icerock.moko.resources.compose.fontFamilyResource
+import dev.icerock.moko.resources.compose.*
 
 @Composable
 internal fun App() {
-    MaterialTheme {
-        val initialString: String = stringResource(MR.strings.hello_world)
-        var text by remember(initialString) { mutableStateOf(initialString) }
-        var counter by remember { mutableStateOf(0) }
-
-        val fileContent: String? by MR.files.some.readTextAsState()
-        val assetContent: String? by MR.assets.asset_file.readTextAsState()
-
-        val textColor: Color = colorResource(MR.colors.textColor)
-
-        Column {
+    MaterialTheme(
+        colors = if (isSystemInDarkTheme()) darkColors() else lightColors()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .background(color = MaterialTheme.colors.background)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Image(
-                modifier = Modifier.size(40.dp),
-                painter = painterResource(MR.images.home_black_18),
+                modifier = Modifier.width(320.dp).height(192.dp),
+                painter = painterResource(MR.images.moko_logo),
                 contentDescription = null
             )
-            Text(
-                text = fileContent.orEmpty()
+
+            var text: String by remember { mutableStateOf("") }
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(top = 16.dp),
+                value = text,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    textColor = MaterialTheme.colors.onBackground
+                ),
+                onValueChange = { text = it }
             )
+
+            val counter: Int = text.length
             Text(
-                text = assetContent.orEmpty()
+                modifier = Modifier.fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                text = stringResource(MR.plurals.chars_count, counter, counter),
+                color = colorResource(MR.colors.textColor),
+                fontFamily = fontFamilyResource(MR.fonts.cormorant.italic)
             )
-            Text(
-                text = stringResource(MR.plurals.items_count, counter),
-                color = textColor,
-            )
-            Button(onClick = {
-                counter++
-                text = "Hello, ${getPlatformName()}"
-            }) {
-                Text(
-                    text = text,
-                    fontFamily = fontFamilyResource(MR.fonts.cormorant.italic)
-                )
+
+            Button(onClick = { text = "Hello, ${getPlatformName()}" }) {
+                Text(text = stringResource(MR.strings.hello_world))
             }
+
+            val fileContent: String? by MR.files.some_file.readTextAsState()
+            Text(
+                modifier = Modifier.padding(top = 16.dp),
+                text = fileContent.orEmpty(),
+                color = MaterialTheme.colors.onBackground
+            )
+
+            val assetContent: String? by MR.assets.some_asset.readTextAsState()
+            Text(
+                modifier = Modifier.padding(top = 16.dp),
+                text = assetContent.orEmpty(),
+                color = MaterialTheme.colors.onBackground
+            )
         }
     }
 }
