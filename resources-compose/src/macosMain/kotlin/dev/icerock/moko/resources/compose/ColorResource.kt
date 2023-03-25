62ba3ebdbe33c6c5b3a3a7536f67718ef131fb50
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2023-03-25 14:35:52 +0600

    review fix

--- resources-compose/src/macosMain/kotlin/dev/icerock/moko/resources/compose/ColorResource.kt
+++ resources-compose/src/macosMain/kotlin/dev/icerock/moko/resources/compose/ColorResource.kt
@@ -6,6 +6,7 @@ package dev.icerock.moko.resources.compose
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import dev.icerock.moko.resources.ColorResource
 import dev.icerock.moko.resources.getNSColor
@@ -16,11 +17,13 @@ actual fun colorResource(resource: ColorResource): Color {
     // TODO https://github.com/icerockdev/moko-resources/issues/443
     //  recompose when appearance changed (now not works in runtime!)
     val darkMode: Boolean = isSystemInDarkTheme()
-    val ciColor: NSColor = resource.getNSColor()
-    return Color(
-        red = ciColor.redComponent.toFloat(),
-        green = ciColor.greenComponent.toFloat(),
-        blue = ciColor.blueComponent.toFloat(),
-        alpha = ciColor.alphaComponent.toFloat()
-    )
+    return remember(resource) {
+        val ciColor: NSColor = resource.getNSColor()
+        Color(
+            red = ciColor.redComponent.toFloat(),
+            green = ciColor.greenComponent.toFloat(),
+            blue = ciColor.blueComponent.toFloat(),
+            alpha = ciColor.alphaComponent.toFloat()
+        )
+    }
 }
