c30a2cb154c4367e87dda2a5e9cf58e700a8471e
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2023-06-04 18:19:41 +0600

    #460 exclude unprocessed resources

--- resources-generator/src/main/kotlin/dev/icerock/gradle/utils/ProcessResourcesUtils.kt
+++ resources-generator/src/main/kotlin/dev/icerock/gradle/utils/ProcessResourcesUtils.kt
@@ -7,11 +7,20 @@ package dev.icerock.gradle.utils
 import dev.icerock.gradle.generator.MRGenerator
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.kotlin.dsl.withType
+import org.gradle.language.jvm.tasks.ProcessResources
 
 fun dependsOnProcessResources(project: Project, sourceSet: MRGenerator.SourceSet, task: Task) {
     project.tasks
         .matching { it.name == sourceSet.name.removeSuffix("Main") + "ProcessResources" }
-        .configureEach {
-            it.dependsOn(task)
+        .withType<ProcessResources>()
+        .configureEach { processResourcesTask ->
+            processResourcesTask.exclude {
+                val path: String = it.file.absolutePath
+                if (path.contains("generated/moko")) return@exclude true
+                if (path.contains("resources/MR")) return@exclude true
+                false
+            }
+            processResourcesTask.dependsOn(task)
         }
 }
