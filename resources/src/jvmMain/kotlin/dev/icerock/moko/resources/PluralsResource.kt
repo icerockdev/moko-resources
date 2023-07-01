6881efc90404d3dfd7efee85c4257857d6cda50b
Lucchetto <lucchetto.tie@live.com>
2023-07-01 23:13:38 +0200

    Add fallback for plural when string of quantity not defined on JVM
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- resources/src/jvmMain/kotlin/dev/icerock/moko/resources/PluralsResource.kt
+++ resources/src/jvmMain/kotlin/dev/icerock/moko/resources/PluralsResource.kt
@@ -4,7 +4,6 @@
 
 package dev.icerock.moko.resources
 
-import com.ibm.icu.text.MessageFormat
 import com.ibm.icu.text.PluralRules
 import com.ibm.icu.util.ULocale
 import java.util.*
@@ -22,7 +21,11 @@ actual class PluralsResource(
         val selectedVariant = pluralRules.select(quantity.toDouble())
         val keyWithQuantity = "$key.$selectedVariant"
 
-        return resourceBundle.getString(keyWithQuantity)
+        return if (resourceBundle.containsKey(keyWithQuantity)) {
+            resourceBundle.getString(keyWithQuantity)
+        } else {
+            resourceBundle.getString("$key.${PluralRules.KEYWORD_OTHER}")
+        }
     }
 
     fun localized(locale: Locale = Locale.getDefault(), quantity: Int): String =
