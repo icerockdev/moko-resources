4dca57537b5c5e0e9eeeed4e8fb280fb3c642f11
Aleksey Mikhailov <aleksey.mikhailov@icerockdev.com>
2023-07-30 19:59:41 +0600

    update kotlin version in resources-gallery

--- samples/resources-gallery/build.gradle.kts
+++ samples/resources-gallery/build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
     }
     dependencies {
         classpath(moko.resourcesGradlePlugin)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
         classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.compose:compose-gradle-plugin:1.4.0")
     }
