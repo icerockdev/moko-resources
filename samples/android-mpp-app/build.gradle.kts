9ab0d0506026017eeb28f23b915a4cc8e17a0beb
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2024-01-14 22:47:08 +0700

    #535 enable config cache in android-mpp sample

--- samples/android-mpp-app/build.gradle.kts
+++ samples/android-mpp-app/build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
     }
     dependencies {
         classpath(moko.resourcesGradlePlugin)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-        classpath("com.android.tools.build:gradle:8.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+        classpath("com.android.tools.build:gradle:8.1.0")
     }
 }
