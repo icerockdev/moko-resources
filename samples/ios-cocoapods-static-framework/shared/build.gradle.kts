f11242ab69813fe8a25ac6bf10e59bd0a2412abe
Konstantin Kolchurin <theunrealkosyak@gmail.com>
2024-02-02 13:32:46 +0700

    #535 add cocoapods with static framework sample

---
+++ samples/ios-cocoapods-static-framework/shared/build.gradle.kts
@@ -0,0 +1,91 @@
+import dev.icerock.gradle.tasks.GenerateMultiplatformResourcesTask
+
+plugins {
+    kotlin("multiplatform")
+    kotlin("native.cocoapods")
+    id("com.android.library")
+    id("org.jetbrains.compose")
+    id("dev.icerock.mobile.multiplatform-resources")
+}
+
+kotlin {
+    androidTarget()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    cocoapods {
+        version = "1.0.0"
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        ios.deploymentTarget = "15"
+        podfile = project.file("../iosApp/Podfile")
+        framework {
+            baseName = "shared"
+            binaryOptions["bundleId"] = "com.share.resources.module"
+
+            isStatic = true
+            export(moko.resources)
+            export("dev.icerock.moko:graphics:0.9.0")
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+                implementation(compose.components.resources)
+                api(moko.resources)
+                api(moko.resourcesCompose)
+            }
+        }
+        named("commonMain") {
+            resources.srcDirs("resources")
+            // other parts
+        }
+        val androidMain by getting {
+            dependencies {
+                api("androidx.activity:activity-compose:1.8.2")
+                api("androidx.appcompat:appcompat:1.6.1")
+                api("androidx.core:core-ktx:1.12.0")
+            }
+        }
+    }
+}
+
+android {
+    compileSdk = (findProperty("android.compileSdk") as String).toInt()
+    namespace = "com.myapplication.common"
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+
+    defaultConfig {
+        minSdk = (findProperty("android.minSdk") as String).toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        jvmToolchain(17)
+    }
+}
+
+multiplatformResources {
+    resourcesPackage.set("com.share.resources.module")
+}
+
+project.tasks.matching {
+    it.name.lowercase().contains("LintModel".lowercase())
+}.configureEach {
+    mustRunAfter(
+        project.tasks.withType<GenerateMultiplatformResourcesTask>().matching {
+            it.name.lowercase().contains("generateMRandroidUnitTest".lowercase())
+        }
+    )
+}
