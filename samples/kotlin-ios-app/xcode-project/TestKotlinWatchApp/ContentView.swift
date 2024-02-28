20bb335c4f28323949caba3b29fd0c9a0d3e4959
Cornelli Fabio <fabio.cornelli@comelit.it>
2024-02-28 15:19:27 +0100

    add test watchOS app target in kotlin-ios-app sample

---
+++ samples/kotlin-ios-app/xcode-project/TestKotlinWatchApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TestKotlinWatchApp
+//
+//  Created by Cornelli Fabio on 28/02/24.
+//  Copyright © 2024 IceRock Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
