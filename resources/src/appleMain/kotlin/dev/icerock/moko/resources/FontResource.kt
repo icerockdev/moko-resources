f9b9a4f24e406f175e899c27ecea7b6c5d8dab1d
Konstantin Kolchurin <theunrealkosyak@gmail.com>
2024-04-17 17:12:05 +0700

    #535 reformat

--- resources/src/appleMain/kotlin/dev/icerock/moko/resources/FontResource.kt
+++ resources/src/appleMain/kotlin/dev/icerock/moko/resources/FontResource.kt
@@ -5,9 +5,12 @@
 package dev.icerock.moko.resources
 
 import cnames.structs.__CFData
+import kotlinx.cinterop.BetaInteropApi
 import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.UnsafeNumber
 import kotlinx.cinterop.alloc
+import kotlinx.cinterop.convert
 import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.ptr
 import kotlinx.cinterop.value
@@ -35,15 +38,15 @@ import platform.darwin.UInt8Var
 
 actual class FontResource(
     val fontName: String,
-    val bundle: NSBundle = NSBundle.mainBundle
+    val bundle: NSBundle = NSBundle.mainBundle,
 ) {
-    @OptIn(ExperimentalForeignApi::class)
+    @OptIn(ExperimentalForeignApi::class, UnsafeNumber::class)
     internal val fontRef: CGFontRef by lazy {
         val fontData: NSData = this.data
         val cfDataRef: CPointer<__CFData>? = CFDataCreate(
             kCFAllocatorDefault,
             fontData.bytes() as CPointer<UInt8Var>,
-            fontData.length.toLong()
+            fontData.length.toLong().convert()
         )
         val dataProvider = CGDataProviderCreateWithCFData(cfDataRef)
         val cgFont = CGFontCreateWithDataProvider(dataProvider)!!
@@ -62,6 +65,7 @@ actual class FontResource(
             ) ?: error("file $fontName not found in $bundle")
         }
 
+    @OptIn(BetaInteropApi::class)
     val data: NSData
         get() {
             val filePath: String = this.filePath
@@ -69,7 +73,7 @@ actual class FontResource(
                 ?: error("can't read $filePath file")
         }
 
-    @OptIn(ExperimentalForeignApi::class)
+    @OptIn(ExperimentalForeignApi::class, UnsafeNumber::class)
     @Throws(NSErrorException::class)
     @Suppress("unused")
     fun registerFont() {
@@ -78,13 +82,23 @@ actual class FontResource(
         // UNCHECKED_CAST - NSString and CFStringRef are toll-free bridged
         @Suppress("CAST_NEVER_SUCCEEDS", "UNCHECKED_CAST")
         val cfStringFilePath = CFBridgingRetain(filePath as NSString) as CFStringRef
-        val cfFontUrlRef = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, cfStringFilePath, kCFURLPOSIXPathStyle, false)
+        val cfFontUrlRef = CFURLCreateWithFileSystemPath(
+            kCFAllocatorDefault,
+            cfStringFilePath,
+            kCFURLPOSIXPathStyle,
+            false
+        )
 
         var nsError: NSError? = null
 
         memScoped {
             val error = alloc<CFErrorRefVar>()
-            if (!CTFontManagerRegisterFontsForURL(cfFontUrlRef, kCTFontManagerScopeProcess, error.ptr)) {
+            if (!CTFontManagerRegisterFontsForURL(
+                    cfFontUrlRef,
+                    kCTFontManagerScopeProcess,
+                    error.ptr
+                )
+            ) {
                 error.value?.let {
                     nsError = CFBridgingRelease(it) as NSError
                 }
