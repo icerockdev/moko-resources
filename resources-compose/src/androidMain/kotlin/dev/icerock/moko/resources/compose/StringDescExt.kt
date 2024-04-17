a5e30ed6054af71e5bf98e2fdedf1ac6959048d0
Konstantin Kolchurin <theunrealkosyak@gmail.com>
2024-04-17 14:08:25 +0700

    #533 configuration change adaptation

--- resources-compose/src/androidMain/kotlin/dev/icerock/moko/resources/compose/StringDescExt.kt
+++ resources-compose/src/androidMain/kotlin/dev/icerock/moko/resources/compose/StringDescExt.kt
@@ -6,11 +6,14 @@ package dev.icerock.moko.resources.compose
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import dev.icerock.moko.resources.desc.StringDesc
 
 @Composable
 actual fun StringDesc.localized(): String {
+    // If recreate context is off
+    LocalConfiguration.current
     val context: Context = LocalContext.current
     return toString(context)
 }
--- resources-compose/src/androidMain/kotlin/dev/icerock/moko/resources/compose/StringResource.kt
+++ resources-compose/src/androidMain/kotlin/dev/icerock/moko/resources/compose/StringResource.kt
@@ -5,7 +5,6 @@
 package dev.icerock.moko.resources.compose
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import dev.icerock.moko.resources.PluralsResource
 import dev.icerock.moko.resources.StringResource
 import dev.icerock.moko.resources.desc.Plural
@@ -16,16 +15,16 @@ import dev.icerock.moko.resources.desc.StringDesc
 
 @Composable
 actual fun stringResource(resource: StringResource): String =
-    StringDesc.Resource(resource).toString(LocalContext.current)
+    StringDesc.Resource(resource).localized()
 
 @Composable
 actual fun stringResource(resource: StringResource, vararg args: Any): String =
-    StringDesc.ResourceFormatted(resource, *args).toString(LocalContext.current)
+    StringDesc.ResourceFormatted(resource, *args).localized()
 
 @Composable
 actual fun stringResource(resource: PluralsResource, quantity: Int): String =
-    StringDesc.Plural(resource, quantity).toString(LocalContext.current)
+    StringDesc.Plural(resource, quantity).localized()
 
 @Composable
 actual fun stringResource(resource: PluralsResource, quantity: Int, vararg args: Any): String =
-    StringDesc.PluralFormatted(resource, quantity, *args).toString(LocalContext.current)
+    StringDesc.PluralFormatted(resource, quantity, *args).localized()
