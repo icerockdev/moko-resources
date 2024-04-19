71501201e416f1640bacc9f60f0f9dad8bf1d1f1
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2024-04-19 21:31:44 +0700

    #648 fix #Preview error

--- samples/kotlin-ios-app/xcode-project/TestKotlinWatchApp/ContentView.swift
+++ samples/kotlin-ios-app/xcode-project/TestKotlinWatchApp/ContentView.swift
@@ -21,6 +21,8 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
