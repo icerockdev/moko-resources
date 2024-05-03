4aa68eaf2976085b50d3bd17a06fb22964e89a79
Konstantin Kolchurin <theunrealkosyak@gmail.com>
2024-05-03 12:07:07 +0700

    #689 fix expect strings, remove useless symbols

--- resources-generator/src/test/kotlin/dev/icerock/gradle/generator/stringsGenerator/XmlStringsToPlatformTest.kt
+++ resources-generator/src/test/kotlin/dev/icerock/gradle/generator/stringsGenerator/XmlStringsToPlatformTest.kt
@@ -13,7 +13,7 @@ class XmlStringsToPlatformTest {
     @Test
     fun newLineAndroid() {
         assertEquals(
-            expected = "first line\\nsecond line",
+            expected = """first line\nsecond line""",
             actual = "first line\nsecond line".convertXmlStringToAndroidLocalization()
         )
     }
@@ -21,7 +21,7 @@ class XmlStringsToPlatformTest {
     @Test
     fun newLineOtherPlatforms() {
         assertEquals(
-            expected = "first line\\nsecond line",
+            expected = """first line\nsecond line""",
             actual = "first line\nsecond line".convertXmlStringToLocalization()
         )
     }
@@ -29,7 +29,7 @@ class XmlStringsToPlatformTest {
     @Test
     fun separateSymbolsAndroid() {
         assertEquals(
-            expected = "\\&quot; \\&apos; % @ * &amp; {}",
+            expected = """\&quot; \&apos; % @ * &amp; {}""",
             actual = "\" ' % @ * &amp; {}".convertXmlStringToAndroidLocalization()
         )
     }
@@ -37,7 +37,7 @@ class XmlStringsToPlatformTest {
     @Test
     fun separateSymbolsOtherPlatforms() {
         assertEquals(
-            expected = "\\\" \\' % @ * & {}",
+            expected = """\" \' % @ * & {}""",
             actual = "\" ' % @ * &amp; {}".convertXmlStringToLocalization()
         )
     }
@@ -45,7 +45,7 @@ class XmlStringsToPlatformTest {
     @Test
     fun textWithApostropheAndroid() {
         assertEquals(
-            expected = "I\\&apos;m bought new monitor with 27 inch\\&apos;s",
+            expected = """I\&apos;m bought new monitor with 27 inch\&apos;s""",
             actual = "I'm bought new monitor with 27 inch's".convertXmlStringToAndroidLocalization()
         )
     }
@@ -53,7 +53,7 @@ class XmlStringsToPlatformTest {
     @Test
     fun textWithApostropheOtherPlatforms() {
         assertEquals(
-            expected = "I\\'m bought new monitor with 27 inch\\'s",
+            expected = """I\'m bought new monitor with 27 inch\'s""",
             actual = "I'm bought new monitor with 27 inch's".convertXmlStringToLocalization()
         )
     }
@@ -61,7 +61,7 @@ class XmlStringsToPlatformTest {
     @Test
     fun textWithXmlTagsAndroid() {
         assertEquals(
-            expected ="Text with &lt;b&gt;bold&lt;\\/b&gt;, &lt;i&gt;italic&lt;\\/i&gt;, &lt;u&gt;underline&lt;\\/u&gt;",
+            expected = """Text with &lt;b&gt;bold&lt;\/b&gt;, &lt;i&gt;italic&lt;\/i&gt;, &lt;u&gt;underline&lt;\/u&gt;""",
             actual = "Text with &lt;b&gt;bold&lt;/b&gt;, &lt;i&gt;italic&lt;/i&gt;, &lt;u&gt;underline&lt;/u&gt;".convertXmlStringToAndroidLocalization(),
         )
     }
@@ -69,16 +69,8 @@ class XmlStringsToPlatformTest {
     @Test
     fun textWithXmlTagsOtherPlatforms() {
         assertEquals(
-            expected ="Text with <b>bold<\\/b>, <i>italic<\\/i>, <u>underline<\\/u>",
+            expected = """Text with <b>bold<\/b>, <i>italic<\/i>, <u>underline<\/u>""",
             actual = "Text with &lt;b&gt;bold&lt;/b&gt;, &lt;i&gt;italic&lt;/i&gt;, &lt;u&gt;underline&lt;/u&gt;".convertXmlStringToLocalization(),
-        )
-    }
-
-    @Test
-    fun singleSymbolsParseOtherPlatforms() {
-        assertEquals(
-            expected = "Text \\n with \\\" \\\' % @ * & {} same inch\\\'s",
-            actual = "Text \n with \" ' % @ * &amp; {} same inch's".convertXmlStringToLocalization()
         )
     }
 }
