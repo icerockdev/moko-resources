6308565ce4506923666f42b5a04b48c6df6d8ca2
Konstantin Kolchurin <theunrealkosyak@gmail.com>
2024-05-17 16:26:53 +0700

    #710 add new string and plurals tests

--- resources-generator/src/test/kotlin/dev/icerock/gradle/generator/stringsGenerator/XmlStringsToPlatformTest.kt
+++ resources-generator/src/test/kotlin/dev/icerock/gradle/generator/stringsGenerator/XmlStringsToPlatformTest.kt
@@ -5,6 +5,7 @@
 package dev.icerock.gradle.generator.stringsGenerator
 
 import dev.icerock.gradle.utils.convertXmlStringToAndroidLocalization
+import dev.icerock.gradle.utils.convertXmlStringToApplePluralLocalization
 import dev.icerock.gradle.utils.convertXmlStringToLocalization
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -29,7 +30,7 @@ class XmlStringsToPlatformTest {
     @Test
     fun separateSymbolsAndroid() {
         assertEquals(
-            expected = """&quot; &apos; % @ * &amp; {}""",
+            expected = """\&quot; \&apos; % @ * &amp; {}""",
             actual = "\" ' % @ * &amp; {}".convertXmlStringToAndroidLocalization()
         )
     }
@@ -45,7 +46,7 @@ class XmlStringsToPlatformTest {
     @Test
     fun textWithApostropheAndroid() {
         assertEquals(
-            expected = """I&apos;m bought new monitor with 27 inch&apos;s""",
+            expected = """I\&apos;m bought new monitor with 27 inch\&apos;s""",
             actual = "I'm bought new monitor with 27 inch's".convertXmlStringToAndroidLocalization()
         )
     }
@@ -71,6 +72,54 @@ class XmlStringsToPlatformTest {
         assertEquals(
             expected = """Text with <b>bold</b>, <i>italic</i>, <u>underline</u>""",
             actual = "Text with &lt;b&gt;bold&lt;/b&gt;, &lt;i&gt;italic&lt;/i&gt;, &lt;u&gt;underline&lt;/u&gt;".convertXmlStringToLocalization(),
+        )
+    }
+
+    @Test
+    fun textWithQuotesAndroid() {
+        assertEquals(
+            expected = """%d count \&quot;%s\&quot;""",
+            actual = "%d count \"%s\"".convertXmlStringToAndroidLocalization()
+        )
+    }
+
+    @Test
+    fun textWithQuotesOtherPlatforms() {
+        assertEquals(
+            expected = """%d count \&quot;%s\&quot;""",
+            actual = "%d count \"%s\"".convertXmlStringToAndroidLocalization()
+        )
+    }
+
+    @Test
+    fun unicodeEmojiAppleTest() {
+        assertEquals(
+            expected = """😈""",
+            actual = "\uD83D\uDE08".convertXmlStringToLocalization()
+        )
+    }
+
+    @Test
+    fun unicodeEmojiApplePluralTest() {
+        assertEquals(
+            expected = """😈""",
+            actual = "\uD83D\uDE08".convertXmlStringToApplePluralLocalization()
+        )
+    }
+
+    @Test
+    fun unicodeEmojiAndroidTest() {
+        assertEquals(
+            expected = """😈""",
+            actual = "\uD83D\uDE08".convertXmlStringToAndroidLocalization()
+        )
+    }
+
+    @Test
+    fun unicodeEmojiOtherPlatformTest() {
+        assertEquals(
+            expected = """😈""",
+            actual = "\uD83D\uDE08".convertXmlStringToLocalization()
         )
     }
 }
