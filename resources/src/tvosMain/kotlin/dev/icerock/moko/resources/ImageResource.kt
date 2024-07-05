0b468bd775f15741768747995fc836210e26692d
Aleksandr Mironov <aleksandr.mironov@ebox.ca>
2024-07-05 19:37:49 -0400

    implemented required expect classes and methods for tvOS

---
+++ resources/src/tvosMain/kotlin/dev/icerock/moko/resources/ImageResource.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2024 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package dev.icerock.moko.resources
+
+import platform.UIKit.UIImage
+
+actual class ImageResource(val assetImageName: String) {
+    fun toUIImage(): UIImage? {
+        return UIImage.imageNamed(name = assetImageName)
+    }
+}
