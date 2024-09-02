266422840ab82cb874437a868dac44c5d5cc3b72
Mitchell Syer <Syer10@users.noreply.github.com>
2024-09-02 12:35:46 -0400

    Update StringExt.kt

--- resources-generator/src/main/kotlin/dev/icerock/gradle/utils/StringExt.kt
+++ resources-generator/src/main/kotlin/dev/icerock/gradle/utils/StringExt.kt
@@ -53,7 +53,7 @@ internal fun String.removeAndroidMirroringFormat(): String {
     //  Remove android format from string
     return replace("""\'""", "'")
         .replace("""\"""", "\"")
-        .replace("""\?""", "?") 
+        .replace("""\?""", "?")
 }
 
 internal fun String.convertXmlStringToAndroidLocalization(): String {
