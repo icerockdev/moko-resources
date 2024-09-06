f5bc03b02dbbda0da1b824be072a2cd0b2f198f2
Konstantin Kolchurin <theunrealkosyak@gmail.com>
2024-09-07 03:43:47 +0700

    #762 fix duplicate of code

--- resources-generator/src/main/kotlin/dev/icerock/gradle/generator/platform/apple/SetupAppleUtils.kt
+++ resources-generator/src/main/kotlin/dev/icerock/gradle/generator/platform/apple/SetupAppleUtils.kt
@@ -160,7 +160,6 @@ internal fun registerCopyFrameworkResourcesToAppTask(
 
     val configName: String = (configMap[configuration]?.name ?: configuration).lowercase()
     // Map any configuration to standard with two types: "Debug/Release" builds
-    val clearConfigurationName: String = when{
     val clearConfigurationName: String = when {
         configName.matches(".*debug.*".toRegex()) -> "debug"
         configName.matches(".*release.*".toRegex()) -> "release"
